7dec25e07a2df73b616bcb44400ef856726fe67e
Renan Fonseca <marcos.renan.cf@gmail.com>
2021-01-17 14:00:13 -0300

    ajustando display para mostrar o resultado e calculando resultado

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -7,7 +7,12 @@ class Calc {
     constructor() {
 
         console.log("Iniciou");
+        this.setDisplay();
         this.initBtnEvents();
+
+        setInterval(()=>{
+            this.setDisplay();
+        }, 1);
        
         
     }
@@ -28,13 +33,40 @@ class Calc {
         this._lastOperator = value;
 
         if(!this.isOperation(value) && !isNaN(value) && this._arrayNumbers.length == 0) {
+
             this._arrayNumbers.push(value);
+            console.log(this._arrayNumbers);
+            
+        } else if(!isNaN(value) && this.isOperation(this._getLastArray())) {
+
+            this._arrayNumbers.push(value);
+            console.log(this._arrayNumbers);
+
+        } else if(!this.isOperation(value)) {
+            
+            let number = this._getLastNumber().toString();
+            let newNumber = number+value;
+            
+            this._arrayNumbers[this._arrayNumbers.length -1] = parseInt(newNumber);
+            console.log(this._arrayNumbers);            
         }
 
-        if(this.isOperation(value) && this._arrayNumbers.length >= 1) {
+        if(this.isOperation(value) && this._arrayNumbers.length >= 1 && !this.isOperation(this._getLastArray())) {
+
             this._arrayNumbers.push(value);
+            console.log(this._arrayNumbers);
+
+        } else if(this.isOperation(value) && this._arrayNumbers.length >= 1 && this.isOperation(this._getLastArray())) {
+            this._changeOperator(value);
+            console.log(this._arrayNumbers);
         }
 
+    }
+
+    setDisplay() {
+
+        let display = document.querySelector("#display");
+        display.innerHTML = this._arrayNumbers.join("");
     }
 
     isOperation(value) {
@@ -54,6 +86,27 @@ class Calc {
         }
 
         return this._lastNumber;
+    }
+
+    _getLastArray() {
+        return this._arrayNumbers[this._arrayNumbers.length -1];
+    }
+
+    _changeOperator(value) {
+        if(this.isOperation(value)) {
+            this._arrayNumbers[this._arrayNumbers.length - 1] = value;
+        }
+    }
+
+    _btnCalcular(value) {
+        let total = eval(value.join(""));
+        this._setResult(total);
+        console.log(total);
+    }
+
+    _setResult(value) {
+        let result = document.querySelector("#result");
+        result.innerHTML = value;
     }
 
     execBtn(value) {
@@ -86,6 +139,10 @@ class Calc {
 
             case '+':
                 this.addOperation('+');
+                break;
+
+            case '=':
+                this._btnCalcular(this._arrayNumbers);
                 break;
             
             default:
