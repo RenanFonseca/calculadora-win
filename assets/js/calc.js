bcad2120629540ebc77832f40dfbde76bf6153bb
Renan Fonseca <marcos.renan.cf@gmail.com>
2021-01-17 12:36:09 -0300

    adicionando função que retorna o ultimo numero do arrayNumeros

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,9 +1,14 @@
 class Calc {
+    
+    _lastOperator = "";
+    _lastNumber = "";
+    _arrayNumbers = [];
 
     constructor() {
 
         console.log("Iniciou");
         this.initBtnEvents();
+       
         
     }
 
@@ -19,14 +24,41 @@ class Calc {
 
     }
 
-    addOperation(value) {
-        console.log(value);
+    addOperation(value) {     
+        this._lastOperator = value;
+
+        if(!this.isOperation(value) && !isNaN(value) && this._arrayNumbers.length == 0) {
+            this._arrayNumbers.push(value);
+        }
+
+        if(this.isOperation(value) && this._arrayNumbers.length >= 1) {
+            this._arrayNumbers.push(value);
+        }
+
+    }
+
+    isOperation(value) {
+
+        return (['+', '-', '*', '/'].indexOf(value) > -1);
+
+    }
+
+    _getLastNumber(i = 1) {
+
+        
+        if(!this.isOperation(this._arrayNumbers[this._arrayNumbers.length - i])) {
+            this._lastNumber = this._arrayNumbers[this._arrayNumbers.length - i]
+        } else {
+            ++i;
+            this._getLastNumber(i);
+        }
+
+        return this._lastNumber;
     }
 
     execBtn(value) {
 
         switch(value) {
-
             case '0':
             case '1':
             case '2':
@@ -38,6 +70,26 @@ class Calc {
             case '8':
             case '9':
                 this.addOperation(parseInt(value));
+                break;  
+            
+            case '÷':
+                this.addOperation('/');
+                break;
+            
+            case 'X':
+                this.addOperation('*');
+                break;
+
+            case '-':
+                this.addOperation('-');
+                break;
+
+            case '+':
+                this.addOperation('+');
+                break;
+            
+            default:
+                console.log(value);
                 break;
         }
 
@@ -45,5 +97,3 @@ class Calc {
 }
 
 window.winCalc = new Calc;
-
-
